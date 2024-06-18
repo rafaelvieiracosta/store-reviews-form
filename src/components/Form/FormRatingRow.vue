1e7c1e30171a05a8b4261e223a6bb996563e3677
rafaelvieiracosta <rvc.vieira@outlook.com>
2024-06-17 21:34:02 -0300

    refactor: classes rating type row

--- src/components/Form/FormRatingRow.vue
+++ src/components/Form/FormRatingRow.vue
@@ -1,38 +1,38 @@
 <template>
-  <ul class="ul">
+  <ul class="rating-row">
     <li>
-      <input type="radio" id="row-option-1" class="input" value="1" v-model="rating" />
-      <label for="row-option-1" class="label">
+      <input type="radio" id="row-option-1" class="rating-row__input" value="1" v-model="rating" />
+      <label for="row-option-1" class="rating-row__label">
         <img src="@/assets/emojis/pouting-face.png" width="32" height="32" />
-        <span class="label-popover left">Terrível</span>
+        <span class="rating-row__label__popover left">Terrível</span>
       </label>
     </li>
     <li>
-      <input type="radio" id="row-option-2" class="input" value="2" v-model="rating" />
-      <label for="row-option-2" class="label">
+      <input type="radio" id="row-option-2" class="rating-row__input" value="2" v-model="rating" />
+      <label for="row-option-2" class="rating-row__label">
         <img src="@/assets/emojis/confused-face.png" width="32" height="32" />
-        <span class="label-popover left">Ruim</span>
+        <span class="rating-row__label__popover left">Ruim</span>
       </label>
     </li>
     <li>
-      <input type="radio" id="row-option-3" class="input" value="3" v-model="rating" />
-      <label for="row-option-3" class="label">
+      <input type="radio" id="row-option-3" class="rating-row__input" value="3" v-model="rating" />
+      <label for="row-option-3" class="rating-row__label">
         <img src="@/assets/emojis/neutral-face.png" width="32" height="32" />
-        <span class="label-popover center">Não muito boa</span>
+        <span class="rating-row__label__popover center">Não muito boa</span>
       </label>
     </li>
     <li>
-      <input type="radio" id="row-option-4" class="input" value="4" v-model="rating" />
-      <label for="row-option-4" class="label">
+      <input type="radio" id="row-option-4" class="rating-row__input" value="4" v-model="rating" />
+      <label for="row-option-4" class="rating-row__label">
         <img src="@/assets/emojis/smiling-face-with-smiling-eyes.png" width="32" height="32" />
-        <span class="label-popover right">Bem legal</span>
+        <span class="rating-row__label__popover right">Bem legal</span>
       </label>
     </li>
     <li>
-      <input type="radio" id="row-option-5" class="input" value="5" v-model="rating" />
-      <label for="row-option-5" class="label">
+      <input type="radio" id="row-option-5" class="rating-row__input" value="5" v-model="rating" />
+      <label for="row-option-5" class="rating-row__label">
         <img src="@/assets/emojis/grinning-face-with-smiling-eyes.png" width="32" height="32" />
-        <span class="label-popover right">Experiência incrível</span>
+        <span class="rating-row__label__popover right">Experiência incrível</span>
       </label>
     </li>
   </ul>
@@ -45,14 +45,14 @@ const rating = ref(5)
 </script>
 
 <style scoped>
-.ul {
+.rating-row {
   margin-top: 16px;
   display: flex;
   gap: 6px;
   padding-bottom: 38px;
   max-width: fit-content;
 }
-.label {
+.rating-row__label {
   position: relative;
   z-index: 0;
   display: block;
@@ -62,7 +62,12 @@ const rating = ref(5)
   border-radius: 50%;
   cursor: pointer;
 }
-.label-popover {
+.rating-row__input {
+  display: none;
+}
+
+/* type row: mensagem */
+.rating-row__label__popover {
   opacity: 0;
   position: absolute;
   top: calc(100% + 8px);
@@ -75,7 +80,7 @@ const rating = ref(5)
   border-radius: 30px;
   transition: 250ms;
 }
-.label-popover::before {
+.rating-row__label__popover::before {
   content: '';
   display: block;
   top: -5px;
@@ -84,38 +89,37 @@ const rating = ref(5)
   height: 5px;
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='5' viewBox='0 0 12 5' fill='none'%3E%3Cpath d='M6 0L12 5H0L6 0Z' fill='%23F2F2F2'/%3E%3C/svg%3E%0A");
 }
-.label-popover.left {
+.rating-row__label__popover.left {
   left: 3px;
 }
-.label-popover.left::before {
+.rating-row__label__popover.left::before {
   left: 16px;
 }
-.label-popover.right {
+.rating-row__label__popover.right {
   right: 3px;
 }
-.label-popover.right::before {
+.rating-row__label__popover.right::before {
   right: 16px;
 }
-.label-popover.center {
+.rating-row__label__popover.center {
   left: 50%;
   transform: translateX(-50%);
 }
-.label-popover.center::before {
+.rating-row__label__popover.center::before {
   left: 50%;
   transform: translateX(-50%);
 }
-.input {
-  display: none;
-}
-input:checked ~ .label {
+
+/* type row: itens selecionados */
+.rating-row__input:checked ~ .rating-row__label {
   background-color: var(--background_color);
   border-color: var(--border_active);
 }
-input:checked ~ .label .label-popover {
+.rating-row__input:checked ~ .rating-row__label .rating-row__label__popover {
   opacity: 1;
 }
 @media (min-width: 1024px) {
-  input:not(:checked) ~ .label:hover {
+  .rating-row__input:not(:checked) ~ .rating-row__label:hover {
     border-color: var(--border);
     box-shadow: var(--shadow);
   }
